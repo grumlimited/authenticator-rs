ecfa195a9607c6cfd00924a453b851a1b417c995
Romain Gallet <romain.gallet@gmail.com>
2020-06-16 22:20:10 +0100

    Gtk - Initial

--- src/main_window.rs
+++ src/main_window.rs
@@ -12,6 +12,17 @@ use glib::Sender;
 use rusqlite::Connection;
 use std::borrow::BorrowMut;
 use std::{thread, time};
+use gtk::Builder;
+
+pub struct EditAccountWindow {
+    pub edit_account: gtk::Box,
+    pub add_accounts_container: gtk::Box,
+    pub edit_account_input_group: gtk::Entry,
+    pub edit_account_input_name: gtk::Entry,
+    pub edit_account_input_secret: gtk::Entry,
+    pub edit_account_cancel: gtk::Button,
+    pub edit_account_save: gtk::Button,
+}
 
 pub struct MainWindow {
     window: gtk::ApplicationWindow,
@@ -21,6 +32,7 @@ pub struct MainWindow {
     stack: Arc<Mutex<RefCell<gtk::Stack>>>,
     accounts_container: gtk::Box,
     pub widgets: Vec<AccountGroupWidgets>,
+    pub edit_account_window: EditAccountWindow,
 }
 
 impl MainWindow {
@@ -49,6 +61,19 @@ impl MainWindow {
             stack: Arc::new(Mutex::new(RefCell::new(stack))),
             accounts_container,
             widgets: vec![],
+            edit_account_window: Self::create_edit_account_window(builder)
+        }
+    }
+
+    fn create_edit_account_window(builder: Builder) -> EditAccountWindow {
+        EditAccountWindow {
+            edit_account: builder.get_object("edit_account").unwrap(),
+            add_accounts_container: builder.get_object("add_accounts_container").unwrap(),
+            edit_account_input_group: builder.get_object("edit_account_input_group").unwrap(),
+            edit_account_input_name: builder.get_object("edit_account_input_name").unwrap(),
+            edit_account_input_secret: builder.get_object("edit_account_input_secret").unwrap(),
+            edit_account_cancel: builder.get_object("edit_account_cancel").unwrap(),
+            edit_account_save: builder.get_object("edit_account_save").unwrap(),
         }
     }
 
