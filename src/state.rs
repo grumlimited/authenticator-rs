be9f7d58214dced6e6f20918c2763f0e7388dc3a
Romain Gallet <romain.gallet@gmail.com>
2020-06-16 13:09:15 +0100

    Gtk - Initial

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,30 @@
 use crate::model::AccountGroup;
+use std::fmt::Debug;
+use serde::export::Formatter;
+
+#[derive(Debug)]
+pub enum StateRs {
+    MainAccounts
+}
+
+impl Default for StateRs {
+    fn default() -> Self {
+        StateRs::MainAccounts
+    }
+}
 
 #[derive(Debug, Default)]
 pub struct State {
     pub groups: Vec<AccountGroup>,
+    pub state_rs: StateRs,
 }
 
 impl State {
     pub fn new() -> Self {
-        Self { ..State::default() }
+        Self {
+            state_rs: StateRs::MainAccounts,
+            ..State::default()
+        }
     }
 
     pub fn add_groups(&mut self, groups: Vec<AccountGroup>) {
