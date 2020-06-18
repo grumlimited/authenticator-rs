dbbf9a51f4455a59cc75c18a335de8799817cbca
Romain Gallet <romain.gallet@gmail.com>
2020-06-19 00:47:12 +0100

    Gtk - Initial

--- src/main_window.rs
+++ src/main_window.rs
@@ -159,6 +159,9 @@ impl MainWindow {
                     ConfigManager::load_account_groups(connection).unwrap()
                 };
 
+                edit_account_window
+                    .input_group.remove_all();
+
                 groups.iter().for_each(|group| {
                     let string = format!("{}", group.id);
                     let entry_id = Some(string.as_str());
