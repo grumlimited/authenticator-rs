4ba4b1a10f8dec4019d0c9587ad44c27fbaf4939
Romain Gallet <romain.gallet@gmail.com>
2020-06-19 00:03:03 +0100

    Gtk - Initial

--- src/ui/add_group.rs
+++ src/ui/add_group.rs
@@ -5,13 +5,17 @@ use gtk::Builder;
 #[derive(Clone, Debug)]
 pub struct AddGroupWindow {
     pub container: gtk::Box,
+    pub cancel_button: gtk::Button,
+    pub save_button: gtk::Button,
 
 }
 
 impl AddGroupWindow {
     pub fn new(builder: Builder) -> AddGroupWindow {
         AddGroupWindow {
-            container: builder.get_object("add_group").unwrap()
+            container: builder.get_object("add_group").unwrap(),
+            cancel_button: builder.get_object("add_group_cancel").unwrap(),
+            save_button: builder.get_object("add_group_save").unwrap(),
         }
     }
 }
