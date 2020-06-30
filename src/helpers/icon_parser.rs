518c43dbc57673176208aea00d4f4ad6010a6eec
Romain Gallet <romain.gallet@gmail.com>
2020-06-30 15:44:29 +0100

    Icon download

--- src/helpers/icon_parser.rs
+++ src/helpers/icon_parser.rs
@@ -1,40 +1,42 @@
 use regex::Regex;
-use reqwest::*;
 use scraper::*;
-use std::sync::mpsc::{channel, Sender};
+use std::sync::mpsc::Sender;
 #[derive(Debug, Clone)]
 pub struct IconParser {}
-use crate::helpers::LoadError::SaveError;
-use reqwest::header::HeaderValue;
-use std::borrow::Cow;
 
-#[derive(Debug, Clone, PartialEq)]
+pub type IconParserResult<T> = std::result::Result<T, IconError>;
+
+#[derive(Debug)]
 pub enum IconError {
     ParsingError,
+    ReqwestError(reqwest::Error),
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct IconParserResult {
+pub struct AccountGroupIcon {
     content: Vec<u8>,
     extension: Option<String>,
 }
 
 impl IconParser {
-    async fn html(sender: Sender<IconParserResult>, url: &str) -> Result<IconParserResult> {
-        let response = reqwest::get(url).await?;
-        let html = response.text().await?;
+    async fn html(
+        sender: Sender<AccountGroupIcon>,
+        url: &str,
+    ) -> IconParserResult<AccountGroupIcon> {
+        let response = reqwest::get(url).await.map_err(IconError::ReqwestError)?;
+        let html = response.text().await.map_err(IconError::ReqwestError)?;
         Self::icon(sender, url, html.as_str()).await
     }
 
     async fn icon(
-        sender: Sender<IconParserResult>,
+        sender: Sender<AccountGroupIcon>,
         url: &str,
         html: &str,
-    ) -> Result<IconParserResult> {
+    ) -> IconParserResult<AccountGroupIcon> {
         let icon_url: std::result::Result<String, IconError> = {
             let document = Html::parse_document(html);
 
-            let selector = Selector::parse(r#"link[rel="icon"]"#).unwrap();
+            let selector = Selector::parse(r#"link[rel="apple-touch-icon"]"#).unwrap();
 
             match document
                 .select(&selector)
@@ -42,19 +44,26 @@ impl IconParser {
                 .next()
                 .and_then(|v| v.value().attr("href"))
             {
+                Some(href) if href.starts_with("/") => Ok(format!("{}/{}", url, href)),
+                Some(href) if href.starts_with("http") => Ok(format!("{}", href)),
                 Some(href) => Ok(format!("{}/{}", url, href)),
                 None => Err(IconError::ParsingError),
             }
         };
 
-        Self::download(sender, icon_url.unwrap().as_str()).await
+        match icon_url {
+            Ok(icon_url) => Self::download(sender, icon_url.as_str()).await,
+            Err(r) => Err(r),
+        }
     }
 
     async fn download(
-        sender: Sender<IconParserResult>,
+        sender: Sender<AccountGroupIcon>,
         icon_url: &str,
-    ) -> Result<IconParserResult> {
-        let response = reqwest::get(icon_url).await?;
+    ) -> IconParserResult<AccountGroupIcon> {
+        let response = reqwest::get(icon_url)
+            .await
+            .map_err(IconError::ReqwestError)?;
         let content_type = response.headers().get("content-type");
 
         let extension = content_type.and_then(|content_type| match content_type.to_str() {
@@ -62,14 +71,14 @@ impl IconParser {
             Err(_) => None,
         });
 
-        let bytes = response.bytes().await?;
+        let bytes = response.bytes().await.map_err(IconError::ReqwestError)?;
 
-        let result = IconParserResult {
+        let result = AccountGroupIcon {
             content: bytes.to_vec(),
             extension,
         };
 
-        sender.send(result.clone());
+        sender.send(result.clone()).expect("Boom!");
 
         Ok(result)
     }
@@ -88,9 +97,7 @@ impl IconParser {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use async_std::task;
-    use tokio::prelude::*;
-    use tokio::runtime::Runtime;
+    use std::sync::mpsc::channel;
 
     #[test]
     fn extension() {
@@ -107,18 +114,18 @@ mod tests {
             .build()
             .unwrap();
 
-        let (sender, receiver) = channel::<IconParserResult>();
+        let (sender, receiver) = channel::<AccountGroupIcon>();
 
         let fut = IconParser::download(
             sender,
-            "https://www.rust-lang.org/static/images/favicon-32x32.png",
+            "https://static.bbci.co.uk/wwhp/1.145.0/responsive/img/apple-touch/apple-touch-180.jpg",
         );
 
         let icon_parser_result = rt.block_on(rt.spawn(fut)).unwrap().unwrap();
-        assert_eq!("png", icon_parser_result.extension.unwrap());
+        assert_eq!("jpeg", icon_parser_result.extension.unwrap());
 
         let icon_parser_result = receiver.recv().unwrap();
-        assert_eq!("png", icon_parser_result.extension.unwrap());
+        assert_eq!("jpeg", icon_parser_result.extension.unwrap());
     }
 
     #[test]
@@ -129,14 +136,14 @@ mod tests {
             .build()
             .unwrap();
 
-        let (sender, receiver) = channel::<IconParserResult>();
+        let (sender, receiver) = channel::<AccountGroupIcon>();
 
-        let fut = IconParser::html(sender, "https://www.rust-lang.org");
+        let fut = IconParser::html(sender, "https://www.bbc.com");
 
         let icon_parser_result = rt.block_on(rt.spawn(fut)).unwrap().unwrap();
-        assert_eq!("png", icon_parser_result.extension.unwrap());
+        assert_eq!("jpeg", icon_parser_result.extension.unwrap());
 
         let icon_parser_result = receiver.recv().unwrap();
-        assert_eq!("png", icon_parser_result.extension.unwrap());
+        assert_eq!("jpeg", icon_parser_result.extension.unwrap());
     }
 }
