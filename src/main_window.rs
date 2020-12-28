e5af1bceced950b692408dc6a9bcdc6b71178a3f
Romain Gallet <romain.gallet@gmail.com>
2020-12-28 23:03:28 +0000

    Async operations

--- src/main_window.rs
+++ src/main_window.rs
@@ -10,10 +10,10 @@ use glib::{Receiver, Sender};
 use gtk::prelude::*;
 use rusqlite::Connection;
 
-use crate::{NAMESPACE, NAMESPACE_PREFIX, ui};
 use crate::helpers::ConfigManager;
 use crate::model::AccountGroup;
 use crate::ui::{AccountsWindow, AddGroupWindow, EditAccountWindow};
+use crate::{ui, NAMESPACE, NAMESPACE_PREFIX};
 
 #[derive(Clone, Debug)]
 pub struct MainWindow {
