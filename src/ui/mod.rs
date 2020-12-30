c549eab6715c7131734fa3d5304c47d72d5b7b22
Romain Gallet <romain.gallet@gmail.com>
2020-12-30 11:36:51 +0000

    No accounts placeholder

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,11 +1,11 @@
 mod accounts_window;
 mod add_group;
 mod edit_account_window;
-mod no_accounts;
 mod form_errors;
+mod no_accounts;
 
-pub use self::no_accounts::*;
 pub use self::accounts_window::*;
 pub use self::add_group::*;
 pub use self::edit_account_window::*;
 pub use self::form_errors::*;
+pub use self::no_accounts::*;
