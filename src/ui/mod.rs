e9e80b893613a8bbce5ca7f240167cc0b0bdf023
Romain Gallet <romain.gallet@gmail.com>
2020-12-30 21:38:58 +0000

    Menus

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -4,6 +4,7 @@ mod edit_account_window;
 mod form_errors;
 pub(crate) mod menu;
 mod no_accounts;
+pub(crate) mod menu;
 
 pub use self::accounts_window::*;
 pub use self::add_group::*;
@@ -11,3 +12,4 @@ pub use self::edit_account_window::*;
 pub use self::form_errors::*;
 pub use self::menu::*;
 pub use self::no_accounts::*;
+pub use self::menu::*;
