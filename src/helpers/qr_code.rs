a1d510ae8d4c78be7d9b0118ac7ae11a4b903ee6
Romain Gallet <romain.gallet@gmail.com>
2021-02-04 21:41:56 +0000

    Regex - secret extractor

--- src/helpers/qr_code.rs
+++ src/helpers/qr_code.rs
@@ -1,59 +1,63 @@
 use regex::Regex;
 
-pub fn extract(qr_code_payload: &str) -> String {
-    let re = Regex::new(r".*secret=(.*?)(&.*)?$").unwrap();
+pub struct QRCode;
 
-    let secret = re.captures(qr_code_payload).and_then(|cap| cap.get(1).map(|secret| secret.as_str()));
+impl QRCode {
+    pub fn extract(qr_code_payload: &str) -> String {
+        let re = Regex::new(r".*secret=(.*?)(&.*)?$").unwrap();
 
-    match secret {
-        Some(v) => v.to_owned(),
-        None => qr_code_payload.to_owned(),
+        let secret = re.captures(qr_code_payload).and_then(|cap| cap.get(1).map(|secret| secret.as_str()));
+
+        match secret {
+            Some(v) => v.to_owned(),
+            None => qr_code_payload.to_owned(),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::QRCode;
 
     #[test]
     fn extract_secret_end() {
         let qr_code_payload = "otpauth://totp/Soomesite:nonfunc@gmail.com?algorithm=SHA1&digits=6&issuer=Somesite&period=30&secret=ABCD";
-        let result = extract(qr_code_payload);
+        let result = QRCode::extract(qr_code_payload);
         assert_eq!("ABCD", result);
     }
 
     #[test]
     fn extract_secret_middle() {
         let qr_code_payload = "otpauth://totp/Soomesite:nonfunc@gmail.com?algorithm=SHA1&digits=6&secret=ABCD&issuer=Somesite&period=30";
-        let result = extract(qr_code_payload);
+        let result = QRCode::extract(qr_code_payload);
         assert_eq!("ABCD", result);
     }
 
     #[test]
     fn extract_secret_beginning() {
         let qr_code_payload = "secret=ABCD&otpauth://totp/Soomesite:nonfunc@gmail.com?algorithm=SHA1&digits=6&secret=ABCD&issuer=Somesite&period=30"; //non sensical
-        let result = extract(qr_code_payload);
+        let result = QRCode::extract(qr_code_payload);
         assert_eq!("ABCD", result);
     }
 
     #[test]
     fn extract_secret_by_itself() {
         let qr_code_payload = "secret=ABCD";
-        let result = extract(qr_code_payload);
+        let result = QRCode::extract(qr_code_payload);
         assert_eq!("ABCD", result);
     }
 
     #[test]
     fn extract_secret_with_ampersands() {
         let qr_code_payload = "&secret=ABCD&";
-        let result = extract(qr_code_payload);
+        let result = QRCode::extract(qr_code_payload);
         assert_eq!("ABCD", result);
     }
 
     #[test]
     fn extract_secret_missing() {
         let qr_code_payload = "ABCD";
-        let result = extract(qr_code_payload);
+        let result = QRCode::extract(qr_code_payload);
         assert_eq!("ABCD", result);
     }
 }
