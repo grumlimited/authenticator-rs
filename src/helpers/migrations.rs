f3ea74ece7ae7fff4e8b955264c05d0c94fbd7d2
Romain Gallet <romain.gallet@gmail.com>
2023-05-18 21:07:21 +0200

    #[warn(clippy::result_large_err)]

--- src/helpers/migrations.rs
+++ src/helpers/migrations.rs
@@ -8,6 +8,7 @@ pub mod runner {
     use refinery::{Error, Report};
     use rusqlite::Connection;
 
+    #[allow(clippy::result_large_err)]
     pub fn run(connection: &mut Connection) -> Result<Report, Error> {
         crate::helpers::refinery::migrations::runner().run(connection)
     }
