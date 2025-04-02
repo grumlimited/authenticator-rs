02c62dd011639c281afbcfce30b64c14123bc0e7
Romain Gallet <1898244+gr211@users.noreply.github.com>
2025-04-03 01:10:40 +0200

    Save button - icon fix
    * Reverted 8380a6537c1eea80d12f24ebac618007142d86b3
    
    * Save button - icon fix
    
    * Save button - icon fix
    
    * Save button - icon fix
    
    ---------
    
    Co-authored-by: Romain Gallet <gr211@users.noreply.github.com>

--- src/ui/edit_account_window.rs
+++ src/ui/edit_account_window.rs
@@ -247,26 +247,32 @@ impl EditAccountWindow {
     pub fn edit_account_buttons_actions(&self, gui: &MainWindow, connection: Arc<Mutex<Connection>>) {
         self.qrcode_action();
 
+        let edit_account = self.clone();
+
         self.cancel_button.connect_clicked(clone!(
+            #[strong]
+            edit_account,
             #[strong]
             gui,
             move |_| {
-                gui.edit_account.reset();
+                edit_account.reset();
                 gui.accounts_window.refresh_accounts(&gui);
             }
         ));
 
         self.save_button.connect_clicked(clone!(
             #[strong]
+            edit_account,
+            #[strong]
             gui,
             move |_| {
-                gui.edit_account.reset_errors();
+                edit_account.reset_errors();
 
-                if let Ok(()) = gui.edit_account.validate(connection.clone()) {
-                    let name = gui.edit_account.input_name.clone();
-                    let secret = gui.edit_account.input_secret.clone();
-                    let account_id = gui.edit_account.input_account_id.clone();
-                    let group = gui.edit_account.input_group.clone();
+                if let Ok(()) = edit_account.validate(connection.clone()) {
+                    let name = edit_account.input_name.clone();
+                    let secret = edit_account.input_secret.clone();
+                    let account_id = edit_account.input_account_id.clone();
+                    let group = edit_account.input_group.clone();
                     let name: String = name.buffer().text();
                     let group_id: u32 = group.active_id().unwrap().as_str().parse().unwrap();
                     let secret: String = {
@@ -279,7 +285,6 @@ impl EditAccountWindow {
                     };
 
                     let filter = gui.accounts_window.get_filter_value();
-
                     let account_id = account_id.buffer().text();
 
                     glib::spawn_future(clone!(
@@ -293,7 +298,7 @@ impl EditAccountWindow {
                         }
                     ));
 
-                    gui.edit_account.reset();
+                    edit_account.reset();
 
                     gui.switch_to(Display::Accounts);
                 }
