cfa4e3c76e26595a844e4ffea0e9e429ffa8a66f
Romain Gallet <gr211@users.noreply.github.com>
2025-11-12 23:08:39 +0100

    1.91.1

--- src/helpers/migrations.rs
+++ src/helpers/migrations.rs
@@ -7,9 +7,13 @@ pub mod refinery {
 pub mod runner {
     use refinery::{Error, Report};
     use rusqlite::Connection;
+    use std::ops::DerefMut;
+    use std::sync::{Arc, Mutex};
 
     #[allow(clippy::result_large_err)]
-    pub fn run(connection: &mut Connection) -> Result<Report, Error> {
+    pub fn run(connection: Arc<Mutex<Connection>>) -> Result<Report, Error> {
+        let mut connection = connection.lock().unwrap();
+        let connection = connection.deref_mut();
         crate::helpers::refinery::migrations::runner().run(connection)
     }
 }
